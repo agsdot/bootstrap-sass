3c625dff695ff32f012d1e5471829fb65fea31c4
agsdot <jonathanlai@eltux.net>
2013-06-02 21:12:02 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapSass::Application.config.secret_token = 'f59587fd404542c4b7bdddf35f55d80f87aa9d9e4351845dfa80d33ee60aee5683c805090969829077501f3812c91dcc1b591eb878772f508a47603a7672df37'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BootstrapSass::Application.config.session_store :cookie_store, key: '_bootstrap-sass_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BootstrapSass::Application.config.session_store :active_record_store
